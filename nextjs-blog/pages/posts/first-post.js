5bbc32a12fe1952e98dcbb7d4e16bf252bc9ec39
Dillon Hess <dhess268@gmail.com>
2023-07-11 00:43:45 -0400

    Navigation using pages api learned

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,3 +1,10 @@
+import Link from 'next/link'
+
 export default function FirstPost() {
-  return <h1>First Post</h1>;
+  return (<>
+            <h1>First Post</h1>
+              <h2>
+                <Link href={'/'}>Back to index</Link>
+              </h2>
+          </>)
 }
